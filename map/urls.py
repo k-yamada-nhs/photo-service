665ec64f16b7f5a56511accecfbc8302782f901c
k-yamada-nhs <k.yamada.nhs@gmail.com>
2019-11-25 16:33:53 +0900

    template edit

--- map/urls.py
+++ map/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+app_name = "map"
 urlpatterns = [
     url('', views.locationmap, name='locationmap'),
     url('ajax_projectdata', views.ajax_projectdata, name='ajax_projectdata')
