ace9ca4e9f22db9fa1957d0e7f892d1436ad367f
k-yamada-nhs <k.yamada.nhs@gmail.com>
2019-11-29 14:53:39 +0900

    1129

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     profileimage = models.ImageField(upload_to='profileimage')
+    comment =models.TextField(max_length=255)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
