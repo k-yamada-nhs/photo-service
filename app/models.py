e57cd51313e8a15dad9dd78858d122c58b43dd89
k-yamada-nhs <k.yamada.nhs@gmail.com>
2019-12-09 15:36:02 +0900

    いろいろアプデ

--- app/models.py
+++ app/models.py
@@ -4,29 +4,60 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Photo(models.Model):
+    
+    # タイトル
     title = models.CharField(max_length=150)
+    # コメント
     comment = models.TextField(blank=True)
-
-
+    # ベース画像
     baseimagepath = models.CharField(max_length=150)
+    # スタイル
     styleimagepath = models.CharField(max_length=150)
-
+    # 出力
     outputimagepath = models.CharField(max_length=150)
-
+    # 緯度
+    latitude = models.CharField(max_length=150, null=True)
+    # 経度
+    longitude = models.CharField(max_length=150, null=True)
     # ユーザーインスタンスを削除した際投稿も削除
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 作成日
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
 class Relationship(models.Model):
+
+    # フォロー
     follow = models.ForeignKey(User, related_name='follow', on_delete=models.CASCADE)
+    # フォロワー
     follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
 
     def __str__(self):
         return "{} : {}".format(self.follow.username, self.follower.username)
 
-class UploadImage(models.Model):
-    image = models.ImageField(upload_to='base')
-    style = models.ImageField(upload_to='style')
+class Comnt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    target = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="photo")
+
+    def __str__(self):
+        return self.text
+
+class Reply(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    target = models.ForeignKey(Comnt, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Like(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Photo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
--- app/views.py
+++ app/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.conf import settings
 import os
-from .models import Photo, Relationship, UploadImage
+from .models import Photo, Relationship, Comnt, Reply, Like
 from django.contrib.auth.decorators import login_required
-from .forms import PhotoForm
+from .forms import PhotoForm, CmntForm
 from django.contrib import messages
 from django.views.decorators.http import require_POST, require_GET
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.core import serializers
-
-#style transfer module import
+import time
+from django.contrib import messages
 from app.modules.faststyle import styletransfer
+from django.views import generic
+from django.db.models import Q
 
-# Create your views here.
+# トップ
+def index(request):
+    return render(request, 'app/index.html')
 
-#style transfer
-
-
+# プロジェクト作成
 @login_required
 def projects_create(request):
     if request.method == "POST":
@@ -28,32 +30,33 @@ def projects_create(request):
             photo.user = request.user
             photo.save()
             messages.success(request, "投稿が完了しました")
-        #return redirect('app:users_detail', pk=request.user.pk)
-
-        hoge = "プロジェクトを保存しました"
-    
-        return HttpResponse(hoge)
+            return redirect('app:users_detail', pk=request.user.pk)
     else:
         form = PhotoForm()
-    
     return render(request, 'app/projects_create.html', {'form': form})
 
-
+# プロジェクト詳細
 def projects_detail(request, pk):
     project = get_object_or_404(Photo, pk=pk)
-    return render(request, 'app/projects_detail.html', {'project': project})
+    comnt = Comnt.objects.filter(target=project)
+    like = Like.objects.filter(post=project)
+    commentform = CmntForm()
 
+    return render(request, 'app/projects_detail.html', {'project': project, 'commentform': commentform, 'cmnt': comnt, 'like': like })
 
+# プロジェクト削除
 @require_POST
 def projects_delete(request, pk):
     project = get_object_or_404(Photo, pk=pk)
     project.delete()
     return redirect('app:users_detail', request.user_id)
 
+# プロジェクト更新
+def projects_update(request, pk):
 
-def index(request):
-    return render(request, 'app/index.html')
+    return render(request, 'app/projects_update.html')
 
+# ユーザー詳細
 def users_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
     projects = user.photo_set.all().order_by('-created_at')
@@ -64,59 +67,85 @@ def users_detail(request, pk):
 
     return render(request, 'app/users_detail.html', {'user': user, 'projects': projects, 'follows': follows, 'followers': followers})
 
-
-# タイムライン
+# タイムライン兼検索
 @login_required
 def users_timeline(request):
-    user = request.user
+    
+    # 検索キーワード受け取り
+    keyword = request.GET.get("title")
 
-    # 自分とフォローしているユーザーの投稿を取得
-    #projects = Photo.objects.all()
+    if keyword:
 
-    return render(request, 'app/users_timeline.html', {'user': user})
+        # キーワード検索
+        projects = Photo.objects.filter(title__icontains=keyword)
+    else:
+        
+        # 自分の作品とフォローしてる人の作品
+        projects = Photo.objects.filter(Q(user=request.user) | Q(user=request.user))
+    
+    return render(request, 'app/users_timeline.html', {'projects': projects})
 
-@require_POST
+# スタイル変換
 def test_ajax(request):
 
-    #ベース画像の受け取り
-    base = request.FILES['image']
-    # style = request.FILES['style']
-
-    p = UploadImage(image=base)
-    p.save()
-
-    #p.image.url
-    #保存した画像に対してstyle transfer
+    base = request.GET.get('base')
+    style = request.GET.get('style')
+   
     path = os.getcwd()
-    styletransfer.ffwd_to_img(path+p.image.url, "out.jpg", "model/scream.ckpt")
+    timestr = time.strftime("%Y%m%d-%H%M%S")
 
-    #画像の保存先のpath
+    # pathの生成
+    output_path = path + "/static/images/out/" + timestr + ".jpg"
+    input_path = path + base
+    model_path = "model/"+ style +".ckpt"
+
+    # style transfer
+    styletransfer.ffwd_to_img(input_path, output_path, model_path)
+
+    # 画像の保存先のpath
     hoge = {
-        "base": p.image.url,
-        # "style": p.style.url,
-        # "output_image": "/pasususususus"
-        
+        "base": base,
+        "style": style,
+        "out": "/static/images/out/" + timestr + ".jpg",
         "msg": "send-ok"
     }
     
     return JsonResponse(hoge)
 
+# リアルタイムスタイル変換
 def realtime_transfer(request):
     return render(request, 'app/realtime_transfer.html')
 
-#検索
-def ajax_post_search(request):
-    keyword = request.GET.get("title")
-
-    if keyword:
-
-        projects_to_json = serializers.serialize("json", Photo.objects.filter(title__icontains=keyword))
-    else:
-        
-        projects_to_json = serializers.serialize("json", Photo.objects.all())
+# コメント用
+def comment_ajax(request, pk):
+    post = get_object_or_404(Photo, pk=pk)
+    form = CmntForm(request.POST or None)
     
-    d = {
-        'projects': projects_to_json,
-    }
+    if request.method == 'POST':
+        comment = form.save(commit=False)
+        comment.target = post
+        comment.user = request.user
+        comment.save()
+        return redirect('app:projects_detail', pk=pk)
 
-    return JsonResponse(d)
+# いいね
+def like(request, pk):
+    post = get_object_or_404(Photo, pk=pk)
+    is_like = Like.objects.filter(user=request.user).filter(post=post).count()
+    # いいね済み
+    if is_like > 0:
+        # 削除
+        messages.warning(request, "いいね済みです")
+        return redirect('app:projects_detail', pk=pk)
+
+    # いいねしてない
+    like = Like()
+    like.user = request.user
+    like.post = post
+    like.save()
+    messages.success(request, "いいねしました")
+    return redirect('app:projects_detail', pk=pk)
+
+
+
+
